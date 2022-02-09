ca02cce3b3c3346859c9b1935c86dd214c6aa5dd
Matthew Maggio <me@techwizmatt.info>
2022-02-09 11:11:14 -0700

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="app" id="app">
-    <vue-headful title="" />
-    <navbar />
+    <vue-headful title=""/>
+    <navbar/>
 
     <transition name="fade" mode="out-in">
-      <router-view class="container" />
+      <router-view class="container"/>
     </transition>
   </div>
 </template>
